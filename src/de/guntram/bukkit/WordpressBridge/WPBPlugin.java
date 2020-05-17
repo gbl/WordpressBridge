1e41cad3fcaf66576f356786b56ff5389918e03b
Guntram Blohm <gbl@guntram.de>
2020-05-17 19:01:13 +0200

    also set password for phpbb3

--- src/de/guntram/bukkit/WordpressBridge/WPBPlugin.java
+++ src/de/guntram/bukkit/WordpressBridge/WPBPlugin.java
@@ -21,7 +21,9 @@ public class WPBPlugin extends JavaPlugin {
     private FileConfiguration config;
     private Logger logger;
 
-    private String host, user, password, database, capabilities, prefix;
+    private String host, user, password, database;
+    private String capabilities, prefix;
+    private String phpbb3db, phpbb3user, phpbb3pass;
     private int port;
     
     private Connection mysql;
@@ -39,6 +41,11 @@ public class WPBPlugin extends JavaPlugin {
         database=config.getString("database", "wordpress");
         prefix=config.getString("prefix", "wp_");
         capabilities=config.getString("capabilities", "a:1:{s:15:\"bbp_participant\";b:1;}");
+        
+        phpbb3db=config.getString("phpbb_db", "phpbb3");
+        phpbb3user=config.getString("phpbb_user", "");
+        phpbb3pass=config.getString("phpbb_pass", "");
+        System.out.println("phpbb3 config: "+phpbb3db+"/"+phpbb3user+"/"+phpbb3pass.replaceAll(".", "*"));
 
         reConnect();
         if (mysql != null) {
@@ -101,14 +108,14 @@ public class WPBPlugin extends JavaPlugin {
 
     private boolean createUserOrSetPassword(String name, String uuid, String password) {
         Statement query;
+        String q="";
         try {
             query=mysql.createStatement();
             ResultSet result=query.executeQuery("select id from "+prefix+"users where user_login='"+name+"'");
             if (result.next()) {
                 // user already exists
                 int id=result.getInt("id");
-                String q="select meta_value from "+prefix+"usermeta where user_id="+id+" and meta_key='minecraftuuid'";
-                logger.log(Level.INFO, q);
+                q="select meta_value from "+prefix+"usermeta where user_id="+id+" and meta_key='minecraftuuid'";
                 ResultSet uuidres=query.executeQuery(q);
                 if (uuidres.next()) {
                     String dbuuid=uuidres.getString("meta_value");
@@ -117,13 +124,14 @@ public class WPBPlugin extends JavaPlugin {
                         return false;
                     }
                 } else {
-                    query.executeQuery("insert into "+prefix+"usermeta (umeta_id, user_id, meta_key, meta_value)"+
-                            "values (null, "+id+", 'minecraftuuid', '"+uuid+"'");
+                    q="insert into "+prefix+"usermeta (umeta_id, user_id, meta_key, meta_value) "+
+                            "values (null, "+id+", 'minecraftuuid', '"+uuid+"')";
+                    query.executeUpdate(q);
                 }
-                query.execute("update "+prefix+"users set user_pass='"+md5Hex(password)+"' where id="+id);
-                return true;
+                q="update "+prefix+"users set user_pass='"+md5Hex(password)+"' where id="+id;
+                query.executeUpdate(q);
             } else {
-                String q="insert into "+prefix+"users("+
+                q="insert into "+prefix+"users("+
                                 "id, user_login, user_pass, user_nicename, user_email, user_url, user_registered, user_activation_key, user_status, display_name"+
                                 ") values ("+
                                 "null, '"+name+"', '"+md5Hex(password)+"', '"+name+"', 'unknown@email', '', now(), '', 0, '"+name+"')";
@@ -132,20 +140,51 @@ public class WPBPlugin extends JavaPlugin {
                 result=query.getGeneratedKeys();
                 result.next();
                 int id=result.getInt(1);
-                PreparedStatement pstmt=mysql.prepareStatement(""+
-                        "insert into "+prefix+"usermeta (umeta_id, user_id, meta_key, meta_value)"+
-                        "values (null, ?, ?, ?)"+
-                "");
+                q="insert into "+prefix+"usermeta (umeta_id, user_id, meta_key, meta_value)"+
+                        "values (null, ?, ?, ?)";
+                PreparedStatement pstmt=mysql.prepareStatement(q);
                 pstmt.setInt(1, id); pstmt.setString(2, "minecraftuuid");       pstmt.setString(3, uuid);       pstmt.executeUpdate();
                 pstmt.setInt(1, id); pstmt.setString(2, "nickname");            pstmt.setString(3, name);       pstmt.executeUpdate();
                 pstmt.setInt(1, id); pstmt.setString(2, "wp_user_level");       pstmt.setString(3, "0");        pstmt.executeUpdate();
                 pstmt.setInt(1, id); pstmt.setString(2, "show_admin_bar_front");pstmt.setString(3, "false");    pstmt.executeUpdate();
                 pstmt.setInt(1, id); pstmt.setString(2, "wp_capabilities");     pstmt.setString(3, capabilities);  pstmt.executeUpdate();
-                return true;
             }
         } catch (SQLException ex) {
             logger.log(Level.SEVERE, null, ex);
+            logger.log(Level.INFO, q);
             return false;
         }
+        System.out.println("phpbb3 config: "+phpbb3db+"/"+phpbb3user+"/"+phpbb3pass.replaceAll(".", "*"));
+        if (!phpbb3pass.isEmpty()) {
+            try {
+                mysql.close();
+                mysql = (Connection) DriverManager.getConnection("jdbc:mysql://"+host+":"+port+
+                        "/"+phpbb3db+"?user="+phpbb3user+"&password="+phpbb3pass);                
+                query=mysql.createStatement();
+                q="select user_id from phpbb_users where username='"+name+"'";
+                ResultSet result=query.executeQuery(q);
+                if (result.next()) {
+                    int id=result.getInt("user_id");
+                    q="update phpbb_users set user_password=MD5('"+password+"') where user_id="+id;
+                    query.executeUpdate(q);
+                } else {
+                    q="insert into phpbb_users "+
+                            "(user_type,group_id,user_permissions,username,username_clean,user_password,user_sig) "+
+                            "values "+
+                            "(0, 2,'','"+name+"', '"+name.toLowerCase()+"', MD5('"+password+"'), '')";
+                    query.executeUpdate(q, Statement.RETURN_GENERATED_KEYS);
+                    result=query.getGeneratedKeys();
+                    result.next();
+                    int id=result.getInt(1);
+                    q="insert into phpbb_user_group values (2, "+id+", 0, 0)";
+                    query.executeUpdate(q);
+                }
+            } catch (SQLException ex) {
+                logger.log(Level.SEVERE, null, ex);
+                logger.log(Level.INFO, q);
+                return false;
+            }
+        }
+        return true;
     }
 }
