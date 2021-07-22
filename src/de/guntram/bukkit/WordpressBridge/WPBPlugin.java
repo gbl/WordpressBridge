6b4df90bcb191c624df4b3fc22cb519bd2a0e9da
Guntram Blohm <gbl@guntram.de>
2021-07-22 20:59:23 +0200

    Use java.sql.Connection instead of com.mysql.jdbc.Connection, I have no idea why that was used in the first place.

--- src/de/guntram/bukkit/WordpressBridge/WPBPlugin.java
+++ src/de/guntram/bukkit/WordpressBridge/WPBPlugin.java
@@ -1,7 +1,6 @@
 package de.guntram.bukkit.WordpressBridge;
 
-// import java.sql.Connection;
-import com.mysql.jdbc.Connection;
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
